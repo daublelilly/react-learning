acd2ca47ff67daae95c11dd7f6e44cd9864ac954
daublelilly <133698996+daublelilly@users.noreply.github.com>
2023-09-14 21:40:00 -0400

    start reducers

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,16 +1,88 @@
+import { useReducer } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
+
+const reducer = (state, action) => {
+
+  if(action.type === 'increment') {
+    return {
+      ...state,
+      count: state.count + 1
+    }
+  } 
+
+  if(action.type === 'change-value-to-add') {
+
+    return {
+      ...state,
+      valueToAdd: action.payload
+    }
+  }
+
+  return state;
+}
 
 function CounterPage({ initialCount }) {
 
-  const { count, increment } = useCounter(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
 
-  return <div>
-    <h1>Count is {count}</h1>
-    <Button onClick={increment}>
-      Increment
-    </Button>
-  </div>
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0
+  });
+
+  const increment = () => {
+
+    dispatch({
+      type: 'increment',
+    });
+  }
+
+  const decrement = () => {
+
+    // setCount(count - 1);
+  }
+
+  const handleChange = (event) => {
+
+    const value = parseInt(event.target.value) || 0;
+
+    dispatch({
+      type: 'change-value-to-add',
+      payload: value
+    });
+  }
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
+  }
+
+  return (
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      
+      <div className="flex flex-row">
+        <Button onClick={increment}>
+          Increment
+        </Button>
+
+        <Button onClick={decrement}>
+          Decrement
+        </Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot</label>
+        <input type="number" value={state.valueToAdd || ""} onChange={handleChange} className="p-1 m-3 bg-gray-50 border border-gray-300" />
+        <Button>Add it</Button>
+      </form>
+    </Panel>
+  );
 }
 
 export default CounterPage;
