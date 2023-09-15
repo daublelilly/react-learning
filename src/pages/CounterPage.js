e7dbd403aab9cfee77e4f847760b0e6befcffc6d
daublelilly <133698996+daublelilly@users.noreply.github.com>
2023-09-15 16:49:49 -0400

    eod

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,24 +2,36 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add-value-to-count';
+
 const reducer = (state, action) => {
 
-  if(action.type === 'increment') {
-    return {
-      ...state,
-      count: state.count + 1
-    }
-  } 
+  switch(action.type) {
 
-  if(action.type === 'change-value-to-add') {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
 
-    return {
-      ...state,
-      valueToAdd: action.payload
-    }
+    case SET_VALUE_TO_ADD:
+      return {
+          ...state,
+          valueToAdd: action.payload,
+        };
+
+    case DECREMENT_COUNT:
+        return {
+          ...state,
+          count: state.count - 1,
+        }
+      
+    default:
+      return state;
   }
-
-  return state;
 }
 
 function CounterPage({ initialCount }) {
@@ -32,16 +44,20 @@ function CounterPage({ initialCount }) {
     valueToAdd: 0
   });
 
+  console.log(state);
+
   const increment = () => {
 
     dispatch({
-      type: 'increment',
+      type: INCREMENT_COUNT,
     });
   }
 
   const decrement = () => {
 
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   }
 
   const handleChange = (event) => {
@@ -49,8 +65,8 @@ function CounterPage({ initialCount }) {
     const value = parseInt(event.target.value) || 0;
 
     dispatch({
-      type: 'change-value-to-add',
-      payload: value
+      type: SET_VALUE_TO_ADD,
+      payload: value,
     });
   }
 
@@ -67,18 +83,18 @@ function CounterPage({ initialCount }) {
       <h1 className="text-lg">Count is {state.count}</h1>
       
       <div className="flex flex-row">
-        <Button onClick={increment}>
-          Increment
-        </Button>
-
-        <Button onClick={decrement}>
-          Decrement
-        </Button>
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
       </div>
 
       <form onSubmit={handleSubmit}>
         <label>Add a lot</label>
-        <input type="number" value={state.valueToAdd || ""} onChange={handleChange} className="p-1 m-3 bg-gray-50 border border-gray-300" />
+        <input
+          type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
         <Button>Add it</Button>
       </form>
     </Panel>
