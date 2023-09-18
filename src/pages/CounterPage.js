116320947cad192885f17242dc002b59a542991d
daublelilly <133698996+daublelilly@users.noreply.github.com>
2023-09-18 16:53:36 -0400

    finish section 18

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import produce from 'immer'; 
 import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
@@ -12,22 +13,21 @@ const reducer = (state, action) => {
   switch(action.type) {
 
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
 
     case SET_VALUE_TO_ADD:
-      return {
-          ...state,
-          valueToAdd: action.payload,
-        };
+      state.valueToAdd = action.payload;
+      return;
 
     case DECREMENT_COUNT:
-        return {
-          ...state,
-          count: state.count - 1,
-        }
+      state.count = state.count - 1;
+      return;
+
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
       
     default:
       return state;
@@ -39,7 +39,7 @@ function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
 
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0
   });
@@ -75,8 +75,7 @@ function CounterPage({ initialCount }) {
     event.preventDefault();
 
     dispatch({
-      type: ADD_VALUE_TO_COUNT,
-      payload: 
+      type: ADD_VALUE_TO_COUNT
     });
   }
 
